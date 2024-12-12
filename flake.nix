687ac71ab1c34b585740749555d2e6913d4908f2
aveltras <romain.viallard@outlook.fr>
2024-12-12 13:15:50 +0100

    tweak flake

--- flake.nix
+++ flake.nix
@@ -15,51 +15,65 @@
       android,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; config.allowUnfree = true; };
-        android-studio = pkgs.androidStudioPackages.stable;
-        android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
-          # Useful packages for building and testing.
-          build-tools-34-0-0
-          cmdline-tools-latest
-          emulator
-          platform-tools
-          platforms-android-34
+    let
+      inherit (nixpkgs.lib) attrValues genAttrs;
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: genAttrs supportedSystems (system: f system);
+    in rec {
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = attrValues self.overlays ++ [(import rust-overlay)];
+        config.allowUnfree = true;
+      });
 
-          # Other useful packages for a development environment.
-          # ndk-26-1-10909125
-          # skiaparser-3
-          # sources-android-34
-        ]);
-      in
-        {
-          packages.default = pkgs.callPackage ./. {};
+      overlays.default = final: prev: {
+        android-tablet-bridge = final.callPackage ./default.nix {};
+      };
 
-          devShells.default =
-            pkgs.mkShell {
-              ANDROID_HOME = "${android-sdk}/share/android-sdk";
-              ANDROID_SDK_ROOT = "${android-sdk}/share/android-sdk";
-              JAVA_HOME = pkgs.jdk.home;
-              buildInputs = with pkgs; [
-                android-sdk
-                android-studio
-                gradle
-                jdk
-                (rust-bin.selectLatestNightlyWith (
-                  toolchain:
-                  toolchain.default.override {
-                    targets = [ "x86_64-unknown-linux-gnu" ];
-                    extensions = [
-                      "rust-analyzer"
-                      "rust-src"
-                    ];
-                  }
-                ))
-              ];
-            };
-        }
-    );
+      packages = forAllSystems (system: rec {
+        inherit (nixpkgsFor.${system}) android-tablet-bridge;
+        default = android-tablet-bridge;
+      });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          android-studio = pkgs.androidStudioPackages.stable;
+          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+            # Useful packages for building and testing.
+            build-tools-34-0-0
+            cmdline-tools-latest
+            emulator
+            platform-tools
+            platforms-android-34
+
+            # Other useful packages for a development environment.
+            # ndk-26-1-10909125
+            # skiaparser-3
+            # sources-android-34
+          ]);
+        in {
+          default = pkgs.mkShell {
+            ANDROID_HOME = "${android-sdk}/share/android-sdk";
+            ANDROID_SDK_ROOT = "${android-sdk}/share/android-sdk";
+            JAVA_HOME = pkgs.jdk.home;
+            buildInputs = with pkgs; [
+              android-sdk
+              android-studio
+              gradle
+              jdk
+              (rust-bin.selectLatestNightlyWith (
+                toolchain:
+                toolchain.default.override {
+                  targets = [ "x86_64-unknown-linux-gnu" ];
+                  extensions = [
+                    "rust-analyzer"
+                    "rust-src"
+                  ];
+                }
+              ))
+            ];
+          };
+        });
+    };
 }
